ca17fd89b8be70b827f83e5d9a938642eb35e834
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2024-10-27 17:47:04 +0800

    Time: 0 ms (100%), Space: 18.9 MB (9.11%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,16 +1,4 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> int:
         nums[:] = sorted(nums)
-        counter = 1
-
-        for i in range(1, len(nums)):
-            if nums[i] == nums[i - 1]:
-                counter = counter + 1
-
-            else:
-                counter = 1
-
-            if counter > len(nums) // 2:
-                return nums[i]   
-
-        return nums[0]   
+        return nums[len(nums) // 2]
