ad70a62ce7f61b768c10111853aedd116331e298
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2024-11-05 15:24:37 +0800

    Time: 3 ms (97.48%), Space: 17.6 MB (40.17%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -6,7 +6,11 @@ class Solution:
         counter = 0
         for i in range(0, len(nums) - 1):
 
-            max_reach = max(max_reach, i + nums[i])
+
+            if max_reach < i + nums[i]:
+                max_reach = i + nums[i]
+            
+            #max_reach = max(max_reach, i + nums[i])
 
             if max_reach >= len(nums) - 1:
                 return counter + 1
