d777a09c88ea09b38eeaa013a7277dea8d5204c7
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-06-01 17:04:16 +0500

    Time: 0 ms (100%), Space: 17.8 MB (43.23%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,37 +3,37 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        output = [0] * (m + n)
 
-        i = 0
-        j = 0
-        k = 0
-
-        while i < m and j < n:
+        """
+        we are using 2 pointers technique here
+        """
+        i = m - 1
+        j = n - 1
+        k = m + n - 1
+        
+        """
+        start backwards to avoid overriding, since nums1 is larger
+        """
+        while i >= 0 and j >= 0:
             if nums1[i] < nums2[j]:
-                output[k] = nums1[i]
-                i += 1
+                nums1[k] = nums2[j]
+                j -= 1
             
-            elif nums1[i] >= nums2[j]:
-                output[k] = nums2[j]
-                j += 1
+            else:
+                nums1[k] = nums1[i]
+                i -= 1
             
-            k += 1
-
-        while i < m:
-            output[k] = nums1[i]
-            i += 1
-            k += 1
-
-        while j < n:
-            output[k] = nums2[j]
-            j += 1
-            k += 1
-            
-
-        nums1[:] = output[:]
+            k -= 1
+        
+        """
+        if we keep skipping nums2 elements, we might need to finish placing them
+        """
+        while j >= 0:
+            nums1[k] = nums2[j]
+            k -= 1
+            j -= 1
+        
         return nums1
-
 
 
 
