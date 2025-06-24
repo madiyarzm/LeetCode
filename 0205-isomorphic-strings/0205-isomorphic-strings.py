430b48705befc318b91a5c7ea66740b7cc522806
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-06-24 20:10:36 +0500

    Time: 3 ms (95.39%), Space: 18.1 MB (17.74%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,6 +1,7 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        
+
+        #if lengths not matching its false
         if len(s) != len(t):
             return False
         
@@ -10,16 +11,24 @@ class Solution:
 
         for i in range(len(s)):
             
+            #if s's letter in map
             if s[i] in mp:
 
+                #but it does not correspond to mapped value (means its duplicate)
                 if mp[s[i]] != t[i]:
                     return False
 
+            #if its first time we see s[i]
             else:
+
+                #did we use t[i] already to map?
                 if t[i] in used_values:
                     return False
                 
+                #if not, map onto it
                 mp[s[i]] = t[i]
+
+                #add unique values of t for further check
                 used_values.add(t[i])
         
         return True
