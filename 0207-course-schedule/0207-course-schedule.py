bf380e804ef76674dbbd7eb4cac0e7bdec19e768
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-08-11 23:17:09 -0700

    Time: 7 ms (46.21%), Space: 19.6 MB (16.44%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,53 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        
+        #first we need to turn these lists into dictionary
+
+        #dictioanry creation comprehension
+        graph = {i: [] for i in range(numCourses)}
+        
+        #assigning prereq for each course (b -> a)
+        for course,prereq in prerequisites:
+            graph[course].append(prereq)
+
+        #dfs 
+        
+        seen = set()
+        cycle = set()
+
+        def dfs(course):
+
+            #base case
+            if course in seen:
+                return False
+            
+            if graph[course] == []:
+                return True
+            
+            seen.add(course)
+
+            for neigbor in graph[course]:
+                if not dfs(neigbor):
+                    return False
+            
+            seen.remove(course)
+            graph[course] = []
+            return True
+
+        
+        #additional measure for a case when there are not connected mini graphs
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+        
+        return True
+            
+                
+
+
+
+
+            
+         
+        
+        dfs(graph, prerequisites[0])
