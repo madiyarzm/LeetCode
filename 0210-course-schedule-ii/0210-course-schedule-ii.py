f2138dccf9179695b693af88103ff5990f6a55ce
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-08-12 00:10:45 -0700

    Time: 0 ms (100%), Space: 19.8 MB (12.52%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,19 +1,19 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        graph = {i:[] for i in range(numCourses)}
+        graph = {i: [] for i in range(numCourses)}
         for course, prereq in prerequisites:
             graph[course].append(prereq)
         
         visited = set()
-        traversal = []
         done = set()
+        order = []
 
         def dfs(course):
 
             if course in visited:
                 return False
-
+            
             if course in done:
                 return True
             
@@ -23,18 +23,16 @@ class Solution:
                 if not dfs(prereq):
                     return False
             
-            visited.remove(course)
             done.add(course)
-            traversal.append(course)
+            visited.remove(course)
+            order.append(course)
 
             return True
         
-
         for crs in range(numCourses):
-            
             if not dfs(crs):
                 return []
-
             
-        
-        return traversal
+        return order
+
+
