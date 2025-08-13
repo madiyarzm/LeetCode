2ba8a6f8d7ebc3d4a8884e9e8e631213bcfa7994
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-08-12 19:28:00 -0700

    Time: 1146 ms (18.38%), Space: 57.3 MB (5%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.py
@@ -0,0 +1,34 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        memo = {}
+
+        def dp(amount, coins):
+
+            if amount == 0:
+                return 0
+
+            if amount < 0:
+                return float('inf')
+
+            if amount in memo:
+                return memo[amount]
+
+            else:
+                answer = float('inf')
+
+                for coin in coins:
+                    subproblem = amount - coin
+
+                    if subproblem < 0:
+                        continue
+
+                    answer = min(answer, dp(subproblem, coins) + 1)
+            
+            memo[amount] = answer
+
+            return answer
+
+        if dp(amount,coins) == float('inf'): return -1
+
+        return dp(amount, coins)  
