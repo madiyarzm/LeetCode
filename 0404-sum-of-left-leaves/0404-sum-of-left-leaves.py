53615e685c5b4fc06c295f8e8f4688f10b1dc709
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-08-21 19:56:11 -0700

    Time: 0 ms (100%), Space: 18.2 MB (30.36%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -7,13 +7,18 @@
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
         
+        #if its null then return 0, our base case for backtracking
         if root == None:
             return 0
         
+        #each call in stack has its own sum, no problem with resetting
         sm = 0
+
+        #if its left node, and it has no children -> left leaf
         if root.left and not root.left.left and not root.left.right:
             sm = sm + root.left.val
         
+        #DFS check both subtrees -> 
         sm = sm + self.sumOfLeftLeaves(root.left)
         sm = sm + self.sumOfLeftLeaves(root.right)
 
