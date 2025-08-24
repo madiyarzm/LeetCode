b9306e49878f042836872910a42fc25962b22475
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-08-23 23:32:49 -0700

    Time: 3 ms (14.05%), Space: 18 MB (13.89%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -2,8 +2,10 @@ import math
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
 
+        #we have to input validate & edge check
         if n <= 0:
             return False
 
+        #logariphms in python are float error prone, thats why, we check this way
         return 2 ** int(math.log(n, 2)) == n
     
