1dd918da155a55299922c143fc7f62e359d85adf
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-08-24 20:00:28 -0700

    Time: 11 ms (25.36%), Space: 19.1 MB (45.98%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        
+
+        #built lists as value for hashmap
+        graph = {i: [] for i in range(len(isConnected))}
+
+        #assigning connections to each city in hashmap
+        for city in range(len(isConnected)):
+            for connection in range(len(isConnected[0])):
+                    if city == connection or not isConnected[city][connection]:
+                        continue
+                    
+                    graph[city].append(connection) 
+
+        
+        visited = set()
+
+        #dfs 
+        def dfs(node):
+
+            #if we didnt explore this city, then go for it
+            if node not in visited:
+                visited.add(node)
+
+                #explore its connection's connections
+                for nei in graph.get(node, 0):
+                    dfs(nei)
+        
+        counter = 0
+
+        #loop through all provinces (all ci)
+        for connections in range(len(isConnected)):
+
+            if connections not in visited:
+                dfs(connections)
+                counter += 1
+        
+        return counter
