1da510e01a19d869dbf25630a4a890d8ec37d632
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-08-31 17:03:33 -0700

    Time: 7 ms (82.85%), Space: 18 MB (19.65%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,16 +3,22 @@ class Solution:
         
         res = []
 
+        #we can solve it using bruteforce/backtracking
         def backtrack(start, combination, total):
-
+            
+            #if we got our target value, just add the combination
             if target == total:
                 res.append(list(combination))
                 return
             
+            #if we over it, then backtrack
             elif target < total:
                 return 
             
+            #to not count similar sequences twice, start from start pointer and further
             for i in range(start, len(candidates)):
+
+                #add number and then pop it, after recursively checking over it
                 combination.append(candidates[i])
                 backtrack(i, combination, total + candidates[i])
                 combination.pop()
