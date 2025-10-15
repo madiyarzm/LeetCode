e5a5226ba9c906de7a4c2b795491d241bb711f0f
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-10-15 17:34:00 -0300

    Time: 0 ms (100%), Space: 17.9 MB (35.84%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,6 +1,8 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         
+
+        #key closing, value opening
         close_to_open = {
             ")": "(",
             "}": "{",
@@ -10,14 +12,18 @@ class Solution:
         stack = []
 
         for bracket in s:
-
+            
+            #if its opening, add to stack
             if bracket not in close_to_open:
                 stack.append(bracket)
 
             else:
+
+                #if stack is not empty ")[" or last added opening is not same type as closing
                 if not stack or close_to_open[bracket] != stack[-1]:
                     return False
                 
+                #else, everything is fine, cancels out
                 stack.pop()
         
         return not stack
