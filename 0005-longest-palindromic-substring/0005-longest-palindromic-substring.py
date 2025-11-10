187e3fddd476ef7268987563864934d8aefecc99
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-11-10 12:41:57 -0300

    Time: 331 ms (44.98%), Space: 17.8 MB (83.16%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -6,15 +6,23 @@ class Solution:
 
         for i in range(len(s)):
             
+            #we expand from the center
             l , r = i, i
+
+            #until we are in boundaries, and expansion is working
             while l >= 0 and r < len(s) and s[l] == s[r]:
+
+                #if current length is longer than updated mx len
                 if (r - l + 1) > resLen:
+
+                    #slice needed substring, [)
                     res = s[l:r+1]
                     resLen = r - l + 1
                 
                 l -= 1
                 r += 1
 
+            #for even length, 
             l, r = i, i + 1
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if (r - l + 1) > resLen:
