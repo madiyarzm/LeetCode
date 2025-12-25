c773281e54dd748fd79f34f64edea5cdd35de720
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-12-24 22:50:44 -0300

    Time: 0 ms (100%), Space: 20.5 MB (97.39%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,34 +1,26 @@
 class MinStack:
 
     def __init__(self):
+        self.minstack = []
         self.stack = []
-        self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-
-        #if min stack is not empty
-        if self.min_stack:
-
-            #append only currently min value to minstack
-            val = min(val, self.min_stack[-1])
-            self.min_stack.append(val)
+        if len(self.stack) == 1:
+            self.minstack.append(val)
         
-        #if empty just add it
         else:
-            self.min_stack.append(val)
+            self.minstack.append((min(self.minstack[-1], val)))
 
-
-    #you have to pop value from both stacks, to not have unexistent val from min_stack
     def pop(self) -> None:
         self.stack.pop()
-        self.min_stack.pop()
+        self.minstack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.min_stack[-1]
+        return self.minstack[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
