d91d4f5eb31c98e708971691515cd19f788b2857
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-12-27 16:25:23 -0300

    Time: 159 ms (5.47%), Space: 26.3 MB (94.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,9 +4,11 @@ class Solution:
         mn = float("inf")
         mx_profit = 0
 
+        #we keep most cheapest stock so far
         for i in range(len(prices)):
             mn = min(mn, prices[i])
 
+            #find max profit, by updating
             mx_profit = max(mx_profit, prices[i] - mn)
         
         return mx_profit
