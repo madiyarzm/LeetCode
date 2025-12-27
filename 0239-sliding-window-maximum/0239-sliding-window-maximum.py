6055db12dc0ce7854a60b95f9e298e4ac5c20014
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-12-27 01:38:19 -0300

    Time: 167 ms (58.93%), Space: 32.7 MB (25.26%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -6,16 +6,21 @@ class Solution:
         l = r = 0
         result = []
 
+        #right pointer inbounds
         while r < len(nums):
-
+            
+            #pop from right, while its non-empty and until its not monotonically decreasing
             while q and nums[q[-1]] < nums[r]:
                 q.pop()
             
+            #when its monotically decreasing add
             q.append(r)
 
+            #check whether current leftmost(max) is inbounds, if not remove from left
             if l > q[0]:
                 q.popleft()
 
+            #at least k sized window, and then start appending, once reached start l+=1
             if (r + 1) >= k:
                 result.append(nums[q[0]])
                 l += 1
