45f5bd211ac034167d6eaf23a49317b0cd5b9191
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2025-12-29 19:27:45 -0300

    Time: 81 ms (59.6%), Space: 20.2 MB (76.87%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -3,18 +3,22 @@ class Solution:
         mx = 0
         zeroes = 0
 
+        #we set up 2 pointers
         l = r = 0
         for r in range(len(nums)):
-
+            
             if nums[r] == 0:
                 zeroes += 1
 
+            #shrink window, until zeroes are in allowed number
             while zeroes > k:
                 if nums[l] == 0:
                     zeroes -= 1
                 
+                #shrink left border
                 l += 1
             
+            #update the maximum
             mx = max(mx, r - l + 1)
-
+        
         return mx
