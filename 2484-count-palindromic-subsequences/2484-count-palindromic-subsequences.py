152e739261dcd277ae97785d34d41c28bc0cc5e4
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2026-01-12 16:34:09 -0300

    Time: 1062 ms (40.99%), Space: 57.6 MB (19.24%) - LeetHub

--- 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.py
+++ 2484-count-palindromic-subsequences/2484-count-palindromic-subsequences.py
@@ -2,29 +2,44 @@ class Solution:
     def countPalindromes(self, s: str) -> int:
         mod = 10 ** 9 + 7
         n = len(s)
-        
+
         def findPrefix(s):
-            cntArr = [0]*10
-            dp = [[[0]*10 for _ in range(10)] for _ in range(n)]
-            for i in range(n) :
+
+            #how many digits we meet accessed by the value of digit
+            cnt_digits = [0] * 10
+
+            #3D DP, stores value of possible pairs, at i,j coordinate for given index
+            dp = [[[0] * 10 for _ in range(10)] for _ in range(n)]
+
+            for i in range(n):
                 num = int(s[i])
-                if i-1 >= 0 :
+                if i - 1 >= 0:
                     for x in range(10):
                         for y in range(10):
-                            dp[i][x][y] = dp[i-1][x][y]
-                            # if curr num is y then we get new xy combo
-                            if y == num : 
-                                dp[i][x][y] += cntArr[x]
-                cntArr[num] += 1
+
+                            #copy forward from previous indexs result
+                            dp[i][x][y] = dp[i - 1][x][y]
+
+                            # if we met, Y from XY pair
+                            if y == num:
+
+                                #add counts of X, since its same as count(X) = count(XY)
+                                dp[i][x][y] += cnt_digits[x]
+                
+                cnt_digits[num] += 1
+
             return dp
         
-        pre = findPrefix(s)
-        suff = findPrefix(s[::-1])[::-1]   
-        # suff[i][x][y] is actually suff[i][y][x] here because of reverse string prefix finding
-        
-        res = 0
-        for i in range(2,n-2):
+        prefix = findPrefix(s)
+        suffix = findPrefix(s[::-1])[::-1]
+
+        result = 0
+        for i in range(2, n - 2):
             for x in range(10):
                 for y in range(10):
-                    res = (res + pre[i-1][x][y] * suff[i+1][x][y]) % mod
-        return res
+
+                    #combinatorically find all possible consequences
+                    #valid pairs up to i, valids pairs after i
+                    result = (result + prefix[i - 1][x][y] * suffix[i + 1][x][y]) % mod
+        
+        return result
