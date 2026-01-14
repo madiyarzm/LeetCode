a8b02ba86c8b6a6e625a1c4bc39342effd44bc00
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2026-01-14 13:35:09 -0300

    Time: 141 ms (91.35%), Space: 41.5 MB (17.73%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -2,40 +2,45 @@ import heapq
 class MedianFinder:
 
     def __init__(self):
-
-        #set up 2 empty lists for an object
-        self.minheap = []
-        self.maxheap = []
+        #1. maxheap -> array
+        self.maxheap = [] # M: O(n / 2)
+        
+        #2. minheap -> array
+        self.minheap = [] # M: O(n / 2)
 
     def addNum(self, num: int) -> None:
+        #maxheap is empty, incoming number is lower than maxheap root
+            #maxheap.add(num)
+        if not self.maxheap or num <= -self.maxheap[0]:
+            heapq.heappush(self.maxheap, num * -1) # T: O(log2n)
         
-        #empty or if it belongs to lower half
-        if len(self.maxheap) == 0 or num <= self.maxheap[0] * -1:
-            heapq.heappush(self.maxheap, num * -1)
-        
+        #else (incoming number is higher than maxheap root) -> 
+            #belongs to minheap
         else:
-            heapq.heappush(self.minheap, num)
+            heapq.heappush(self.minheap, num)   # T: O(log2n)
         
-
-        #rebalancing, 2 cases: when maxheap too large
-        if len(self.maxheap) >= len(self.minheap) + 2:
-            heapq.heappush(self.minheap, self.maxheap[0] * -1)
-            heapq.heappop(self.maxheap)
+        #maxheap > minheap + 1
+            #pop the root of maxheap -> minheap (heapifyng)
+        if len(self.maxheap) > len(self.minheap) + 1:
+            heapq.heappush(self.minheap, -heapq.heappop(self.maxheap)) # T: O(log2n)
         
-        #if minheap is larget than maxheap
-        elif len(self.maxheap) < len(self.minheap):
-            heapq.heappush(self.maxheap, self.minheap[0] * -1)
-            heapq.heappop(self.minheap)
-            
+        #minheap > maxheap 
+            #pop the root of minheap -> maxheap (heapifying)
+        if len(self.minheap) > len(self.maxheap):
+            heapq.heappush(self.maxheap, -heapq.heappop(self.minheap)) # T: O(log2n)
 
-    def findMedian(self) -> float:
 
-        #consider even and odd cases
-        if (len(self.maxheap) + len(self.minheap)) % 2 == 0:
-            return (self.maxheap[0] * -1 + self.minheap[0]) / 2
+    def findMedian(self) -> float: #T: O(1)
         
-        else:
+        #if length of the maxheap > minheap (+1)
+            #pop maxheaps root, return 
+        if len(self.maxheap) > len(self.minheap):
             return self.maxheap[0] * -1
+        
+        #length are equal, 
+            #pop minheap roots, and maxheaps root, add them and divide by 2. (float)
+        else:
+            return (self.maxheap[0] * -1 + self.minheap[0]) / 2
 
 
 # Your MedianFinder object will be instantiated and called as such:
