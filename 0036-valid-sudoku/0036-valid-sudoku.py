b4757db0af8ace3364f429956bdc144f246cc453
Madiyar Zhunussov <148677506+madiyarzm@users.noreply.github.com>
2026-07-03 23:47:18 -0700

    Time: 4 ms (47.77%), Space: 19.4 MB (34.45%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,38 +1,31 @@
+from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(9)]
-        columns = [set() for _ in range(9)]
-        box_index = 0
         
-        box_set = [set() for _ in range(9)]
-        
+        #create 9 sets for row and columns
+        rows = [set() for i in range(9)]
+        cols = [set() for i in range(9)]
+
+        #create default dictionary for automatic set creation
+        grid = defaultdict(set)
+
         for i in range(9):
             for j in range(9):
+                if board[i][j] == ".": #skip if its empty
+                    continue
                 
-                if board[i][j] != '.':
-                    if board[i][j] in rows[i]:
-                        return False
-                    
-                    else:
-                        rows[i].add(board[i][j])
+                val = board[i][j]
+                grid_key = (i // 3, j // 3) #get the grid key as a tuple of row and col position of the box
+
+                if val in rows[i] or val in cols[j]: 
+                    return False
                 
-                if board[j][i] != '.':
-                    if board[j][i] in columns[i]:
-                        return False
-                    
-                    else:
-                        columns[i].add(board[j][i])
-        
-        for i in range(9):
-            for j in range(9):
+                rows[i].add(val)
+                cols[j].add(val)
                 
-                box_index = (i // 3) * 3 + j // 3
-                
-                if board[i][j] != '.':
-                    if board[i][j] in box_set[box_index]:
-                        return False
-                    
-                    else:
-                        box_set[box_index].add(board[i][j])
-        
+                if val in grid[grid_key]: #(grid_i, grid_j) -> set()
+                    return False
+
+                grid[grid_key].add(val)
+    
         return True
